8a470dd9ee472ec4563ca997d06150d62679f0ef
eacaps <eacaps@gmail.com>
2016-04-14 10:57:45 -0400

    updated server base

--- actioncable/lib/action_cable/server/base.rb
+++ actioncable/lib/action_cable/server/base.rb
@@ -37,19 +37,29 @@ module ActionCable
         connections.each(&:close)
 
         @mutex.synchronize do
+          puts "@mutex sync restart start"
           worker_pool.halt if @worker_pool
 
           @worker_pool = nil
+          puts "@mutex sync restart end"
         end
       end
 
       # Gateway to RemoteConnections. See that class for details.
       def remote_connections
-        @remote_connections || @mutex.synchronize { @remote_connections ||= RemoteConnections.new(self) }
+        @remote_connections || @mutex.synchronize {
+          puts "@mutex remote_connections start"
+          @remote_connections ||= RemoteConnections.new(self)
+          puts "@mutex remote_connections end"
+        }
       end
 
       def event_loop
-        @event_loop || @mutex.synchronize { @event_loop ||= config.event_loop_class.new }
+        @event_loop || @mutex.synchronize {
+          puts "@mutex event_loop start"
+          @event_loop ||= config.event_loop_class.new
+          puts "@mutex event_loop end"
+        }
       end
 
       # The worker pool is where we run connection callbacks and channel actions. We do as little as possible on the server's main thread.
@@ -64,22 +74,33 @@ module ActionCable
       # the db connection pool and block while they wait for other workers to release their connections. Use a smaller worker pool or a larger
       # db connection pool instead.
       def worker_pool
-        @worker_pool || @mutex.synchronize { @worker_pool ||= ActionCable::Server::Worker.new(max_size: config.worker_pool_size) }
+        @worker_pool || @mutex.synchronize {
+          puts "@mutex worker_pool start"
+          @worker_pool ||= ActionCable::Server::Worker.new(max_size: config.worker_pool_size)
+          puts "@mutex worker_pool end"
+        }
       end
 
       # Requires and returns a hash of all of the channel class constants, which are keyed by name.
       def channel_classes
+        puts "in channel_classes #{@channel_classes}"
         @channel_classes || @mutex.synchronize do
+          puts "@mutex channel_classes start"
           @channel_classes ||= begin
             config.channel_paths.each { |channel_path| require channel_path }
             config.channel_class_names.each_with_object({}) { |name, hash| hash[name] = name.constantize }
           end
+          puts "@mutex channel_classes end"
         end
       end
 
       # Adapter used for all streams/broadcasting.
       def pubsub
-        @pubsub || @mutex.synchronize { @pubsub ||= config.pubsub_adapter.new(self) }
+        @pubsub || @mutex.synchronize {
+          puts "@mutex pubsub start"
+          @pubsub ||= config.pubsub_adapter.new(self)
+          puts "@mutex pubsub end"
+        }
       end
 
       # All of the identifiers applied to the connection class associated with this server.
